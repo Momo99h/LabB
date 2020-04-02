bf542b5c83e5ed3765fd22446c9eb69cec4a04f3
Momo <Momo@DESKTOP-59ILJE1>
2020-04-02 18:52:03 +0200

    Server login/registration

--- ilParoliereLabB/src/main/java/com/universita/ilparolierelabb/server/ServerRegistration.java
+++ ilParoliereLabB/src/main/java/com/universita/ilparolierelabb/server/ServerRegistration.java
@@ -33,11 +33,11 @@ public class ServerRegistration extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 614, Short.MAX_VALUE)
+            .addGap(0, 447, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 381, Short.MAX_VALUE)
+            .addGap(0, 348, Short.MAX_VALUE)
         );
 
         pack();

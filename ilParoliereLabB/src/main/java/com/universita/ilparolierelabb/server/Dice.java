4fa8af1f141acf110b87fa1e9788bf8165812e66
andreagirola <andreagirola@mini-di-andrea.lan>
2020-04-03 11:04:33 +0200

    Refactor variabili

--- ilParoliereLabB/src/main/java/com/universita/ilparolierelabb/server/Dice.java
+++ ilParoliereLabB/src/main/java/com/universita/ilparolierelabb/server/Dice.java
@@ -6,14 +6,15 @@
 package com.universita.ilparolierelabb.server;
 
 import java.lang.*;
+
 /**
  *
  * @author andreagirola
  */
 public class Dice {
-    private int min=1; 
-    private int max=6; 
-    private int id; 
+    private int _min=1; 
+    private int _max=6; 
+    private int _id; 
     private String[] _faces;
     
     
@@ -21,19 +22,19 @@ public class Dice {
     //gli si passa un id (numero del dado da 1 a 16)
     //e un array di lettere corrispondenti al dado in base alle specifiche 
     public Dice(int id, String[] face){ 
-        this.id = id; 
+        this._id = id; 
         this._faces = face; 
         
     }
     
     //getter
     public int getDiceId(){
-        return this.id; 
+        return this._id; 
     }
     
     //ritorna una faccia del dado
     public String getRandomFace(){
-        int rand = (int) (Math.random() * ((max - min) + 1)) + min; 
+        int rand = (int) (Math.random() * ((_max - _min) + 1)) + _min; 
         return this._faces[rand]; 
     }
     
